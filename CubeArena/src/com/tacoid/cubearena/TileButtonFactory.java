a782996704e95bdf9ad81baf1b4c776047700135
Addict7 <nicolasfloquet@gmail.com>
2013-01-08 19:37:35 +0100

    Warnings

--- CubeArena/src/com/tacoid/cubearena/TileButtonFactory.java
+++ CubeArena/src/com/tacoid/cubearena/TileButtonFactory.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
